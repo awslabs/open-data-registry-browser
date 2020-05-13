3d8c8eedc4093767176743a184b20410b569033d
jflasher <jflasher@amazon.com>
2020-05-13 13:45:03 -0400

    Updating robots.txt for staging

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -13,7 +13,8 @@ if [[ "$CODEBUILD_WEBHOOK_HEAD_REF" == "refs/heads/master" && ${CODEBUILD_SOURCE
 elif [[ "$CODEBUILD_WEBHOOK_HEAD_REF" == "refs/heads/staging" && ${CODEBUILD_SOURCE_VERSION:0:3} != "pr/" ]]; then
     echo Creating restrictive robots.txt and deploying dist/ to staging S3 bucket.
 
-    echo "User-agent: *\nDisallow: /" > dist/robots.txt
+    echo "User-agent: *" > dist/robots.txt
+    echo "Disallow: /" >> dist/robots.txt
 
 	# Sync files to S3, removing files that no longer exist
 	aws s3 sync dist/ s3://registry-staging.opendata.aws/ --delete
